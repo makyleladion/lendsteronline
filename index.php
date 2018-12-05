505439c4c4d0ae18f17afb7b1eb3081e5c3df103
Makyle Ladion <makyleladion@gmail.com>
2018-12-05 13:56:10 +0800

    Removed popluar places and replaced download mobile app section

--- index.php
+++ index.php
@@ -64,109 +64,27 @@
 	</div>
 </div>
 
-
-<!-- Content
-================================================== -->
 <div class="container">
-	<div class="row">
-
-		<div class="col-md-12">
-			<h3 class="headline centered margin-top-75">
-				Popular Categories
-				<span>Browse <i>the most desirable</i> categories</span>
-			</h3>
+	<div class="row get-app-section">
+		<div class="col-md-5">
+			<img class="get-app" src="assets/images/get-app2.png">
 		</div>
-
+		<div class="col-md-7">
+			<h2 class="get-app-header1">Lendster Online in your mobile</h2>
+			<h3 class="get-app-header2">Get your app, it's the fastest way to borrow money online.</h3>
+			<div class="col-md-4">
+				<a href="#">
+					<img class="get-app-store" src="assets/images/get-app-store.png">
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#">
+					<img class="get-app-store" src="assets/images/get-google-play.png">
+				</a>
+			</div>				
+		</div>
 	</div>
 </div>
-
-
-<!-- Categories Carousel -->
-<div class="fullwidth-carousel-container margin-top-25">
-	<div class="fullwidth-slick-carousel category-carousel">
-
-		<!-- Item -->
-		<div class="fw-carousel-item">
-
-			<!-- this (first) box will be hidden under 1680px resolution -->
-			<div class="category-box-container half">
-				<a href="listings-half-screen-map-grid-1.html" class="category-box" data-background-image="assets/images/category-box-01.jpg">
-					<div class="category-box-content">
-						<h3>Hotels</h3>
-						<span>64 listings</span>
-					</div>
-					<span class="category-box-btn">Browse</span>
-				</a>
-			</div>
-
-			<div class="category-box-container half">
-				<a href="listings-half-screen-map-grid-1.html" class="category-box" data-background-image="assets/images/category-box-02.jpg">
-					<div class="category-box-content">
-						<h3>Shops</h3>
-						<span>14 listings</span>
-					</div>
-					<span class="category-box-btn">Browse</span>
-				</a>
-			</div>
-		</div>
-
-		<!-- Item -->
-		<div class="fw-carousel-item">
-			<div class="category-box-container">
-				<a href="listings-half-screen-map-grid-1.html" class="category-box" data-background-image="assets/images/category-box-03.jpg">
-					<div class="category-box-content">
-						<h3>Events</h3>
-						<span>67 listings</span>
-					</div>
-					<span class="category-box-btn">Browse</span>
-				</a>
-			</div>
-		</div>
-
-		<!-- Item -->
-		<div class="fw-carousel-item">
-			<div class="category-box-container">
-				<a href="listings-half-screen-map-grid-1.html" class="category-box" data-background-image="assets/images/category-box-04.jpg">
-					<div class="category-box-content">
-						<h3>Fitness</h3>
-						<span>27 listings</span>
-					</div>
-					<span class="category-box-btn">Browse</span>
-				</a>
-			</div>
-		</div>
-
-		<!-- Item -->
-		<div class="fw-carousel-item">
-			<div class="category-box-container">
-				<a href="listings-half-screen-map-list.html" class="category-box" data-background-image="assets/images/category-box-05.jpg">
-					<div class="category-box-content">
-						<h3>Nightlife</h3>
-						<span>22 listings</span>
-					</div>
-					<span class="category-box-btn">Browse</span>
-				</a>
-			</div>
-		</div>
-
-		<!-- Item -->
-		<div class="fw-carousel-item">
-			<div class="category-box-container">
-				<a href="listings-half-screen-map-list.html" class="category-box" data-background-image="assets/images/category-box-06.jpg">
-					<div class="category-box-content">
-						<h3>Eat & Drink</h3>
-						<span>130 listings</span>
-					</div>
-					<span class="category-box-btn">Browse</span>
-				</a>
-			</div>
-		</div>
-
-	</div>
-</div>
-<!-- Categories Carousel / End -->
-
-
 
 <!-- Fullwidth Section -->
 <section class="fullwidth margin-top-65 padding-top-75 padding-bottom-70" data-background-color="#f8f8f8">
